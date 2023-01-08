db5242be10302dd025ed00cd179f9eb728c527d4
kkrisyo <kkrisyo@gmail.com>
2023-01-08 21:18:29 +0100

    Dodaj pliki projektów.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+#if WINDOWS
+        blazorWebView.BlazorWebViewInitializing += BlazorWebView_BlazorWebViewInitializing;
+        blazorWebView.BlazorWebViewInitialized += BlazorWebView_BlazorWebViewInitialized;
+#endif
+    }
+#if WINDOWS
+    private void BlazorWebView_BlazorWebViewInitializing(object sender, Microsoft.AspNetCore.Components.WebView.BlazorWebViewInitializingEventArgs e)
+    {
+        string userDataFolder = @"C:\ProgramData\Blaz";
+        string preferencesFile = Path.Combine(userDataFolder, @"EBWebView\Default", @"Preferences");
+        string webView2Language = "de";
+        Environment.SetEnvironmentVariable("WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS", $"--lang={webView2Language}");
+        Environment.SetEnvironmentVariable("WEBVIEW2_USER_DATA_FOLDER", userDataFolder);
+        if (File.Exists(preferencesFile))
+            File.Delete(preferencesFile);
+    }
+    private void BlazorWebView_BlazorWebViewInitialized(object sender, Microsoft.AspNetCore.Components.WebView.BlazorWebViewInitializedEventArgs e)
+    {
+        e.WebView.CoreWebView2.Settings.AreDefaultContextMenusEnabled = true;
+    }
+#endif
+}
---
+++ MauiApp1/wwwroot/blockrightclick.js
@@ -0,0 +1,14 @@
+﻿window.addEventListener('contextmenu', function (e) {
+    console.log(e.srcElement.tagName);
+    if (!isTextInput(e.srcElement)) {
+        e.preventDefault();
+    }
+}, false);
+
+function isTextInput(ele) {
+    let tagName = ele.tagName;
+    if (tagName === "INPUT" || tagName === "TEXTAREA") {
+        return true
+    }
+    return false;
+}
